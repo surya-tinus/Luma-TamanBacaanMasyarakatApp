f75c29f75cb5a18d38991438697e177692ba381e
surya-tinus <suryanovenskytinus@gmail.com>
2025-10-04 23:43:45 +0700

    Commit 3 - Homepage + Fetch Image Google Books API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,5 +48,8 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.14.2")
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
 }
